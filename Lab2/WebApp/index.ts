90df0f2aee02b9a7cd7d2603116d9e16263111d4
Emanuel <110196@fhwn.ac.at>
2020-10-29 15:33:11 +0100

    Added WebApp

---
+++ Lab2/WebApp/index.ts
@@ -0,0 +1,27 @@
+import * as express from 'express';
+
+export class Server {
+  private app: express.Express;
+
+  private serverName: string = 'TestServer';
+
+  public constructor() {
+    this.app = express();
+    this.app.use(this.logMiddleware.bind(this));
+
+    this.app.get('**',
+      (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        res.end('Hello\n');
+      }
+    );
+
+    this.app.listen(3000, () => console.log('Server started on port 3000!'));
+  };
+
+  private logMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
+    console.log(this.serverName + ': ' + req.url);
+    next();
+  };
+};
+
+new Server();
